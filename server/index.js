3f78792a46391e2f55f0104203c7a9755197fafd
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-27 03:36:31 -0400

    proj structure

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { pool } from "./database/index.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
 pool.query("SELECT * from user", (err, rows, fields) => {
-  console.log(rows);
+    console.log(rows);
 });
+
+app.use(errorHandler);
 
 app.listen(5000, () => {
   console.log("running on port 5000");
