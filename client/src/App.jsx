9d1ba39d0aa4a3d89885383776d877bf23aa540e
DawitSishu <dawitalemayhu55@gmail.com>
2023-07-31 08:41:24 -0400

    signup starting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import SignUp from './Components/Signup';
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#FFFF00",
+    },
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 768, 
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+});
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <BrowserRouter>
+    <Routes>
+        <Route path='/signup' element={<SignUp />} />
+    </Routes>
+    </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
