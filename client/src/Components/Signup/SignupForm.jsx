7b237233cea5adc215908f1440d4451f38edb0e3
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-01 02:54:12 -0400

    basic signup form

--- client/src/Components/Signup/SignupForm.jsx
+++ client/src/Components/Signup/SignupForm.jsx
@@ -4,9 +4,9 @@ import {
   Box,
   Select,
   MenuItem,
-  InputLabel,
+  FormHelperText,
 } from "@mui/material";
-import { useForm,Controller } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import React, { useState } from "react";
 import IconButton from "@mui/material/IconButton";
 import Visibility from "@mui/icons-material/Visibility";
@@ -14,18 +14,31 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import InputAdornment from "@mui/material/InputAdornment";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import { AccountCircle } from "@mui/icons-material";
-import EventIcon from '@mui/icons-material/Event';
-// import { DatePicker, LocalizationProvider } from '@mui/lab';
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import AdapterDateFns from '@mui/lab/AdapterDateFns';
-// import { Link } from "react-router-dom";
-// import 'react-datepicker/dist/react-datepicker.css';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import EventIcon from "@mui/icons-material/Event";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+
+import { TextField, Autocomplete } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+
+const names = [
+  "Humaira Sims",
+  "Santiago Solis",
+  "Dawid Floyd",
+  "Mateo Barlow",
+  "Samia Navarro",
+  "Kaden Fields",
+  "Genevieve Watkins",
+  "Mariah Hickman",
+  "Rocco Richardson",
+  "Harris Glenn"
+];
 
 function SignUpForm(props) {
-  const [showPassword, setShowPassword] = React.useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [selectedValue, setSelectedValue] = useState("student");
+
 
   const {
     register,
@@ -38,13 +51,12 @@ function SignUpForm(props) {
       data.email
     );
     if (validEmail) {
-      // props.onSubmit(data)
+      const data2 = {...data, role : selectedValue}
+      console.log(data2);
     } else {
       alert(`${data.email} is not a valid email`);
     }
   };
-  const [selectedValue, setSelectedValue] = useState("student");
-
   const handleChange = (event) => {
     setSelectedValue(event.target.value);
   };
@@ -57,7 +69,7 @@ function SignUpForm(props) {
       component="form"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <Typography variant="h5">Create a New Account</Typography>
+      <Typography variant="h5" align="center">Create a New Account</Typography>
       <br />
       <Typography color="red" variant="h7">
         {props.err}
@@ -76,65 +88,76 @@ function SignUpForm(props) {
       <br /> <br />
       {selectedValue === "teacher" ? (
         <>
-          <OutlinedInput
-            fullWidth
-            sx={{
-              border: "0.5px solid white",
-            }}
-            placeholder="Subject"
-            id="subject"
-            {...register("subject", { required: "Subject can't be empty" })}
-            startAdornment={
-              <InputAdornment position="start">
-                <AccountCircle />
-              </InputAdornment>
-            }
+        <Controller
+        name="subjects"
+        control={control}
+        rules={{ required: 'Please select at least one subject' }} // Add any validation rules as needed
+        render={({ field }) => (
+          <Autocomplete
+            {...field}
+            multiple
+            value={field.value || []} // Set the initial value to an empty array
+            options={names}
+            getOptionLabel={(option) => option}
+            onChange={(event, value) => field.onChange(value)} // Handle onChange to update the value
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant="outlined"
+                label="Select Subjects"
+                placeholder="Select Subjects"
+              />
+            )}
+            renderOption={(props, option, { selected }) => (
+              <MenuItem {...props} key={option} value={option}>
+                {option}
+                {selected ? <Box component="span" color="info.main">&#10003;</Box> : null}
+              </MenuItem>
+            )}
           />
-          <br />
-          {errors.dob && (
-            <Typography variant="h7">{errors.dob.message}</Typography>
-          )}
-          <br />
+        )}
+      />
+      {errors.subjects && <Typography variant="h7" color="red">{errors.subjects.message}</Typography>}
+      <br />
         </>
       ) : (
         selectedValue === "student" && (
           <>
             <Controller
-                    name="checkInDate"
-                    control={control}
-                    rules={{ required: "Check-In Date can't be empty" }}
-                    render={({ field }) => (
-                      <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DatePicker
-                          label="Check-In Date"
-                          value={field.value || null}
-                          onChange={(date) => field.onChange(date)}
-                          textField={(params) => (
-                            <OutlinedInput
-                              {...params}
-                              fullWidth
-                              id="checkInDate"
-                              type="text"
-                              InputProps={{
-                                ...params.InputProps,
-                                startAdornment: (
-                                  <InputAdornment position="start">
-                                    <EventIcon />
-                                  </InputAdornment>
-                                ),
-                              }}
-                            />
-                          )}
-                          sx={{ width: '100%' }}
-                          error={!!errors.checkInDate}
-                        />
-                        {errors.checkInDate && (
-                          <FormHelperText error>{errors.checkInDate.message}</FormHelperText>
-                        )}
-                      </LocalizationProvider>
+              name="birth_date"
+              control={control}
+              rules={{ required: "Birth Date can't be empty" }}
+              render={({ field }) => (
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="Birth Date"
+                    value={field.value || null}
+                    onChange={(date) => field.onChange(date)}
+                    textField={(params) => (
+                      <OutlinedInput
+                        {...params}
+                        fullWidth
+                        id="birth_date"
+                        type="text"
+                        InputProps={{
+                          ...params.InputProps,
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <EventIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
                     )}
+                    sx={{ width: "100%" }}
+                    error={!!errors.birth_date}
                   />
-            <br />
+                </LocalizationProvider>
+              )}
+            />
+             {errors.birth_date && <Typography variant="h7" color="red">{errors.birth_date.message}</Typography>}
+      <br />
+      <br />
           </>
         )
       )}
@@ -143,9 +166,9 @@ function SignUpForm(props) {
         sx={{
           border: "0.5px solid white",
         }}
-        placeholder="Username"
-        id="username"
-        {...register("username", { required: "Username can't be empty" })}
+        placeholder="Full Name"
+        id="full_name"
+        {...register("full_name", { required: "full_name can't be empty" })}
         startAdornment={
           <InputAdornment position="start">
             <AccountCircle />
@@ -154,7 +177,7 @@ function SignUpForm(props) {
       />
       <br />
       {errors.email && (
-        <Typography variant="h7">{errors.email.message}</Typography>
+        <Typography variant="h7" color="red">{errors.email.message}</Typography>
       )}
       <br />
       <OutlinedInput
@@ -173,7 +196,7 @@ function SignUpForm(props) {
       />
       <br />
       {errors.email && (
-        <Typography variant="h7">{errors.email.message}</Typography>
+        <Typography variant="h7" color="red">{errors.email.message}</Typography>
       )}
       <br />
       <OutlinedInput
@@ -199,7 +222,7 @@ function SignUpForm(props) {
       />
       <br />
       {errors.password && (
-        <Typography variant="h7">{errors.password.message}</Typography>
+        <Typography variant="h7" color="red">{errors.password.message}</Typography>
       )}
       <br />
       <Button type="submit" variant="contained">
