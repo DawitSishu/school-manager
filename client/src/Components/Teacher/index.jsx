084a7e2ffe00bcc52fa7e858b81dd9f0cb8752fa
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-03 04:51:35 -0400

    teacher route basic in progress

--- client/src/Components/Teacher/index.jsx
+++ client/src/Components/Teacher/index.jsx
@@ -1,45 +1,171 @@
-import React, {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-
+import { useState, useEffect } from "react";
+import {
+  Toolbar,
+  ListItemText,
+  ListItemIcon,
+  ListItemButton,
+  ListItem,
+  List,
+  IconButton,
+  Drawer,
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Typography,
+} from "@mui/material";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const BASE_URI_MAIN = "http://localhost:5000/api/users/";
 
+const drawerWidth = 240;
 
-const index = () => {
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState("Update");
   const navigate = useNavigate();
-  useEffect(()=>{
-    const check = async () => {
-      let token = localStorage.getItem("token");
-      let role = localStorage.getItem("role");
-      if (!role || !token){
-        navigate("/");
-      }
-      let config = {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      }
-      try {
-        const result = await axios.post(BASE_URI_MAIN,{role,},config);
-        if (result.data != 'teacher'){
-          navigate('/')
-        }
-        return;
-      } catch (error) {
-        // alert("Eroor : Try agaain")
-        navigate("/");
-      }
-    }
-    check();
-},[])
 
-  
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+  const handleItemClick = (text) => {
+    setSelectedItem(text); // Update the selected item
+  };
 
+  //   useEffect(()=>{
+  //     const check = async () => {
+  //       let token = localStorage.getItem("token");
+  //       let role = localStorage.getItem("role");
+  //       if (!role || !token){
+  //         navigate("/");
+  //       }
+  //       let config = {
+  //         headers: {
+  //           'Authorization': `Bearer ${token}`
+  //         }
+  //       }
+  //       try {
+  //         const result = await axios.post(BASE_URI_MAIN,{role,},config);
+  //         if (result.data != 'teacher'){
+  //           navigate('/')
+  //         }
+  //         return;
+  //       } catch (error) {
+  //         // alert("Eroor : Try agaain")
+  //         navigate("/");
+  //       }
+  //     }
+  //     check();
+  // },[])
+
+  const drawer = (
+    <div>
+      <Toolbar>
+        <Typography>some Teacher</Typography>
+      </Toolbar>
+      <Divider />
+      <List>
+        {["Update", "My Classes", "Get students", "Input Mark"].map(
+          (text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton onClick={() => handleItemClick(text)}>
+                {" "}
+                {/* Add onClick handler */}
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          )
+        )}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <div>index</div>
-  )
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            Teacher Panel
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      >
+        <Toolbar />
+        <Typography paragraph>{selectedItem}</Typography>
+      </Box>
+    </Box>
+  );
 }
 
-export default index
+export default ResponsiveDrawer;
