9f26ace61996d8040036362413169f74c77be0de
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-22 05:06:50 -0400

    the hero section is compleete

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -1,25 +1,42 @@
 import React from "react";
 import { Grid, Typography } from "@mui/material";
-import img from "../../assets/1.jpeg";
+// import img from "../../assets/1.jpeg";
+import img from "../../assets/nds1.webp";
 import "./style.css";
 
 const Hero = () => {
   return (
     <Grid
-    container
-    spacing={0}
-    direction="column"
-    alignItems="center"
-    justifyContent="center"
-    sx={{ 
-      minHeight: '80vh', 
-      backgroundImage: `url(${img})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-    }}
-  >
-      <Typography align="center" variant="h2" color="yellow" className="hero-text">Notre Dame School</Typography>
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{
+        minHeight: "80vh",
+        backgroundImage: `url(${img})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="home2-overlay"></div>
+      <Typography
+        align="center"
+        variant="h2"
+        color="#FFFF00"
+        className="hero-text"
+      >
+        Notre Dame School
+      </Typography>
+      <Typography
+        align="center"
+        variant="h3"
+        color="#FFFF00"
+        className="hero-text"
+      >
+        Knowledge is Power
+      </Typography>
     </Grid>
   );
 };
