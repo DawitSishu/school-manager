e5a5c0ef0d53f25944485964ce862464f211b3eb
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-22 04:58:34 -0400

    hero section started

--- client/src/Components/Home/Hero.jsx
+++ client/src/Components/Home/Hero.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Grid, Typography } from "@mui/material";
+import img from "../../assets/1.jpeg";
+import "./style.css";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <Grid
+    container
+    spacing={0}
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{ 
+      minHeight: '80vh', 
+      backgroundImage: `url(${img})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    }}
+  >
+      <Typography align="center" variant="h2" color="yellow" className="hero-text">Notre Dame School</Typography>
+    </Grid>
+  );
+};
 
-export default Hero
+export default Hero;
