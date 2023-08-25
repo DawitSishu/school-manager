530c246063069f0bba4cb46c581101aa3ae9593c
DawitSishu <dawitalemayhu55@gmail.com>
2023-08-25 07:49:55 -0400

    student can now update data

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -14,7 +14,7 @@ export const studentProfile = asyncHandler(async (req, res) => {
   res.json(result[0][0]);
 });
 
-//@desc updates the password for teacher
+//@desc updates the password for student
 //@route PUT /api/student/profile/update
 //@access private
 export const updatePass = asyncHandler(async (req, res) => {
@@ -26,6 +26,20 @@ export const updatePass = asyncHandler(async (req, res) => {
     `UPDATE students SET password = ? WHERE student_id = ?`,
     [newpassword, student_id]
   );
-res.json({msg:"Password updated successfully!!"});
+  res.json({ msg: "Password updated successfully!!" });
 });
 
+//@desc input the profile for student
+//@route post /api/student/profile/input
+//@access private
+export const inputProfile = asyncHandler(async (req, res) => {
+  const { student_id } = req.user;
+  const { password, birth_date, gender } = req.body;
+  const newpassword = await bcrypt.hash(password, 10);
+  const result = await pool.query(
+    `UPDATE students SET password = ?, date_of_birth = ?, gender = ?, updated = 1 WHERE student_id = ?`,
+    [newpassword, birth_date, gender, student_id]
+  );
+
+  res.json({ msg: "Profile updated successfully!" });
+});
--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -3,13 +3,18 @@ import userAuthChecker from "../middlewares/userAuthCheaker.js";
 import {
   studentProfile,
   updatePass,
+  inputProfile,
 } from "../controllers/studentController.js";
 
 const studentRouter = express.Router();
 
 //view stud info
 studentRouter.get("/profile", userAuthChecker, studentProfile);
+
 // update password
 studentRouter.put("/profile/update", userAuthChecker, updatePass);
+
+// input profile info
+studentRouter.post("/profile/input", userAuthChecker, inputProfile);
 
 export default studentRouter;
