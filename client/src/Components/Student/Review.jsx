8b564fa01de0e2d6464289c2718c456fadbe991b
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-09 09:45:51 -0400

    edits

--- client/src/Components/Student/Review.jsx
+++ client/src/Components/Student/Review.jsx
@@ -1,9 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const URI = "http://localhost:5000/api/student/myteachers";
 
 const Review = () => {
+
+  const [teachers, setTeachers] = useState(null)
+
   let token = localStorage.getItem("token");
   let config = {
     headers: {
@@ -15,6 +18,7 @@ const Review = () => {
       const response = await axios.get(URI, config);
       // let teach = JSON.parse(response.data.teachers);
       console.log(response.data);
+      setTeachers(response.data);
     } catch (error) {
       console.log(error);
     }
