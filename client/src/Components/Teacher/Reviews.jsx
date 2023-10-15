4ad54dd4b71ac9560dbe222c01265b2888ecda6d
DawitSishu <dawitalemayhu55@gmail.com>
2023-10-15 14:03:32 -0400

    the rating message componenet is liked by me as of now

--- client/src/Components/Teacher/Reviews.jsx
+++ client/src/Components/Teacher/Reviews.jsx
@@ -7,29 +7,48 @@ import {
   Avatar,
   Grid,
   Box,
+  Rating,
 } from "@mui/material";
 
 const Reviews = () => {
   const reviews = [
-    { message: "Great service, I'll definitely come back!" },
-    { message: "Awesome experience. Loved the food!" },
-    { message: "Fantastic place for a family dinner." },
+    { message: "Great service, I'll definitely come back!", rating: "3" },
+    { message: "Awesome experience. Loved the food!", rating: "2" },
+    { message: "Fantastic place for a family dinner.", rating: "1.5" },
   ];
+
+  const primaryAvatarStyle = {
+    backgroundColor: "#FFFF00",
+    color: "#000",
+  };
+
+  const cardStyle = {
+    border: "2px solid #FFFF00",
+    borderRadius: "10px",
+    margin: "10px",
+  };
 
   return (
     <div>
-      <Typography variant="h4" gutterBottom>
+      <Typography variant="h4" align="center">
         Student Reviews
       </Typography>
       <Grid container spacing={2}>
         {reviews.map((review, index) => (
           <Grid item xs={12} sm={6} md={4} key={index}>
-            <Card className="review-card">
-              <CardHeader avatar={<Avatar className="review-avatar" />} />
+            <Card style={cardStyle}>
+              <CardHeader
+                avatar={<Avatar style={primaryAvatarStyle}></Avatar>}
+              />
               <CardContent>
-                <Typography variant="body1" className="review-message">
-                  {review.message}
-                </Typography>
+                <Typography variant="body1">{review.message}</Typography>
+                <Box>
+                  <Rating
+                    value={parseFloat(review.rating)}
+                    readOnly
+                    precision={0.5}
+                  />
+                </Box>
               </CardContent>
             </Card>
           </Grid>
